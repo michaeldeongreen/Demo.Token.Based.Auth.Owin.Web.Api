3b96b4e296bb2a31d9511556102e711e2e772936
michaeldeongreen <michael.green@architectinginnovation.com>
2016-12-10 21:16:54 -0600

    *Authentication Context Created

---
+++ src/Demo.Token.Based.Auth.Owin.Web.Api.Repository/AuthenticationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Token.Based.Auth.Owin.Web.Api.Repository
+{
+    public class AuthenticationContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthenticationContext() : base("AuthenticationContext")
+        {
+        }
+    }
+}
